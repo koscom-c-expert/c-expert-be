71f7097148b73bbe1ca7c605ab7377dcb4e85352
hanbikan <fchopinof99@gmail.com>
2025-02-01 11:46:34 +0900

    Add UpdateStockRequest

---
+++ src/main/java/com/koscom/cexpert/dto/UpdateStockRequest.java
@@ -0,0 +1,16 @@
+package com.koscom.cexpert.dto;
+
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Range;
+
+@Data
+@RequiredArgsConstructor
+public class UpdateStockRequest {
+    @Size(min = 1, max = 20)
+    private String name;
+
+    @Range(min = 0)
+    private double currentPrice;
+}
