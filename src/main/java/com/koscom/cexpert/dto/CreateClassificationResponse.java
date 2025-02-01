234b61122f72d2b7091a2df1bf95232ae463f10b
hsacog <hsacog@gmail.com>
2025-02-01 19:56:14 +0900

    Add classification api

---
+++ src/main/java/com/koscom/cexpert/dto/CreateClassificationResponse.java
@@ -0,0 +1,13 @@
+package com.koscom.cexpert.dto;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Data
+@RequiredArgsConstructor
+public class CreateClassificationResponse {
+    private final List<StockCategory> stockCategories;
+    private final List<Stock> stocks;
+}
