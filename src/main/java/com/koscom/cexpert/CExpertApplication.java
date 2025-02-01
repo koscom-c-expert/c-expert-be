204d3e0d421e875aa208212bd22fd414d03fd504
hanbikan <fchopinof99@gmail.com>
2025-02-01 11:19:30 +0900

    first commit

---
+++ src/main/java/com/koscom/cexpert/CExpertApplication.java
@@ -0,0 +1,13 @@
+package com.koscom.cexpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CExpertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CExpertApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koscom/cexpert/CExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koscom.cexpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CExpertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
