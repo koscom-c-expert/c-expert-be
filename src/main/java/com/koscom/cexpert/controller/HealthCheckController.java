27af29c9eebe158b0144ee762d4129356ff1e887
yujin <hyj5419@naver.com>
2025-02-04 10:14:31 +0900

    헬스체크 컨트롤러 추가

--- src/main/java/com/koscom/cexpert/controller/HealthCheckController.java
+++ src/main/java/com/koscom/cexpert/controller/HealthCheckController.java
@@ -2,12 +2,13 @@ package com.koscom.cexpert.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HealthCheckController {
 
     @GetMapping("/health")
-    public String healthCheck() {
+    public @ResponseBody String healthCheck() {
         return "{\"status\": \"UP\"}";
     }
 }
